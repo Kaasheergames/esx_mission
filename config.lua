f21286eaa218fc9b064951d5a4bcb2f8edcfa6a9
Kaasheergames <121974307+Kaasheergames@users.noreply.github.com>
2023-02-26 00:10:23 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,50 @@
+Config = {}
+
+Config.Developer = true
+Config.Locations = {
+
+    {x = -231.7128, y =311.6248, z = 92.16626},
+    {x = -752.0413, y = 368.2673, z = 87.86913},
+    {x = -367.7654, y = -1525.632, z = 27.7573},
+    {x = 1185.369, y = -1358.03, z = 34.95679},
+    {x = 1369.597, y = -734.9691, z = 67.2329},
+    {x = 2726.86, y = 1357.797, z = 24.52393},
+    {x = 3575.709, y = 3769.046, z = 29.92107},
+    {x = -570.5073,y =  5344.351, z = 70.24323},
+
+
+}
+
+Config.NPCAmounts = 3
+Config.PedAppearance = 'g_m_y_mexgoon_01'
+
+Config.StartNPCAppearance = 'a_m_m_trampbeac_01'
+Config.NPCStart = vector3(-799.1818, -893.6425, 19.857) 
+Config.NPCHeading = 30 
+Config.NPCAnimation = true
+Config.StartAmount = 50000
+Config.PropItem = "prop_mil_crate_01"
+
+Config.OpenItem = 'lockpick'
+Config.Reward = 'goldbar'
+
+Config.Items = {
+    {
+        name = "pistol_barrel",
+        limit = 3,
+        minReward = 0,
+        maxReward = 2
+    },
+    {
+        name = "pistol_magazine",
+        limit = 5,
+        minReward = 1,
+        maxReward = 3
+    },
+    {
+        name = "pistol_grip",
+        limit = 3,
+        minReward = 0,
+        maxReward = 2
+    }
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+game "gta5"
+lua54 'yes'
+
+name "esx_mission"
+description "Missions with NPC's"
+author "Kaasheergames - https://github.com/Kaasheergames"
+
+client_scripts {
+	'client/client.lua'
+}
+
+shared_scripts {
+	'config.lua'
+}
+
+server_scripts {
+	'server/server.lua'
+}
+
